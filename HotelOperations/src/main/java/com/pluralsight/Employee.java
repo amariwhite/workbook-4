ec0c7f8e2bb1160cae714b6b80b60a55877fb953
Amari White <amariwhite25@gmail.com>
2025-05-05 16:02:40 -0400

    Hotel Exercise part 1

---
+++ HotelOperations/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class Employee {
+   private String
+   private String
+   private String
+   private double
+   private double
+}
---
+++ HotelOperations/src/main/java/com/pluralsight/HotelApp.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class HotelApp {
+    public static void main(String[] args) {
+
+    }
+}
